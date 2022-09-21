2a06e5579d9bcd189a261b4b2c655c88009f078c
coding-james <mail.stroud@gmail.com>
2022-09-21 12:38:26 +0100

    added data to customers and menu items

--- vegan_restaurant.sql
+++ vegan_restaurant.sql
@@ -39,3 +39,38 @@ ALTER TABLE customers MODIFY post_code VARCHAR(10) NOT NULL; #Modify post_code t
 
 ALTER TABLE customers MODIFY subscribe BOOLEAN DEFAULT false NOT NULL; #Modify subscribe to NOT NULL
 #DROP DATABASE qa_restaurant; #delete database
+
+#Adding Data
+USE qa_restaurant; #identify which database
+
+#Add Customers
+INSERT INTO customers(cust_name, email, phone_number, address_1, address_2, town_city, post_code, delivery_info, subscribe, marketing_email, marketing_text, marketing_post) VALUES("Elizabeth", "queen@royalfamily.uk", "07000000001", "Buckingham Palace","","London","SW1A 1AA","Watch out for the corgis",TRUE,TRUE,FALSE,TRUE);
+INSERT INTO customers(cust_name, email, phone_number, address_1, address_2, town_city, post_code, delivery_info, subscribe, marketing_email, marketing_text, marketing_post) VALUES("Boris", "pm@gov.uk", "07000000002", "10 Downing Street","","London","SW1A 2AA","",1,0,1,0);
+INSERT INTO customers(cust_name, email, phone_number, address_1, address_2, town_city, post_code, delivery_info, subscribe, marketing_email, marketing_text, marketing_post) VALUES("Paddington Bear", "paddington@bear.uk", "07000000003", "Paddington Station","","London","W2 1HQ","",FALSE,FALSE,FALSE,FALSE);
+INSERT INTO customers(cust_name, email, phone_number, address_1, address_2, town_city, post_code, delivery_info, subscribe, marketing_email, marketing_text, marketing_post) VALUES("Nelson", "nelson@column.uk", "07000000004", "Nelson's Column","Trafalger Street","London","WC2","",TRUE,TRUE,TRUE,TRUE);
+INSERT INTO customers(cust_name, email, phone_number, address_1, address_2, town_city, post_code, delivery_info, subscribe, marketing_email, marketing_text, marketing_post) VALUES("Peter Pan", "peter@pan.uk", "07000000005", "Hyde Park Street","","London","W2 2UH","",0,0,0,0);
+
+#Add Menu Items
+INSERT INTO menu_items(item_name, item_desc, allergens, price, calories, availability) VALUES("Jambalaya","Plant-based dish with rice and beans cooked in a single pot","Peanuts",8,324,True);
+INSERT INTO menu_items(item_name, item_desc, allergens, price, calories, availability) VALUES("Chilli","Plant-based chilli served with Sweet Potato","",7.5,400,True);
+INSERT INTO menu_items(item_name, item_desc, allergens, price, calories, availability) VALUES("Vegan Lobster","Carrots shapped to look like a lobster","",6.50,200,True);
+INSERT INTO menu_items(item_name, item_desc, allergens, price, calories, availability) VALUES("Give Peas a Chance","Peas!","",3,100,True);
+INSERT INTO menu_items(item_name, item_desc, allergens, price, calories, availability) VALUES("Toast Sandwhich","Slice of toast between two slices of bread, seasoned with salt and pepper","gluten",3.5,450,False);
+
+#Modify Records
+UPDATE customers SET cust_name="Charles", email="king@royalfamily.uk", delivery_info="" WHERE cust_id=1;
+UPDATE customers SET cust_name="Liz" WHERE cust_id=2;
+UPDATE menu_items SET item_name="Hot Chilli", item_desc="Spicy plant-based chilli served with Sweet Potato and Sour Cream" Where menu_item_id=2;
+
+#Delete Records
+DELETE FROM customers WHERE cust_id=4;
+DELETE FROM menu_items WHERE menu_item_id=3;
+
+#Add additional records
+INSERT INTO menu_items(item_name, item_desc, allergens, price, calories, availability) VALUES("Not Mushroom for this Burger","Mushroom Burger with triple cooked chips","Gluten",9.5,632,True);
+INSERT INTO customers(cust_name, email, phone_number, address_1, address_2, town_city, post_code, delivery_info, subscribe, marketing_email, marketing_text, marketing_post) VALUES("William", "PrinceOfWales@royalfamily.uk", "07000000006", "Buckingham Palace","","London","SW1A 1AA","",0,0,0,0);
+
+SHOW TABLES; #Show Tables
+DESCRIBE customers; #Show Columns, Datatypes, Constraints for tables
+SELECT * FROM customers; #Show customer records
+SELECT * FROM menu_items; #Show menu items records
