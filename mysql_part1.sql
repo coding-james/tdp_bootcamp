ce89f04e4f19e9955b8e469da4c69934cd3a9b6a
coding-james <mail.stroud@gmail.com>
2022-09-21 10:08:32 +0100

    Altered and Modified the Customer Table

--- mysql_part1.sql
+++ mysql_part1.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE qa_restaurant; # creates the database
 USE qa_restaurant; # identifies which table you are going to use
+
 CREATE TABLE customers(
 cust_id INT UNIQUE NOT NULL AUTO_INCREMENT,
 cust_name VARCHAR(100) NOT NULL,
@@ -7,6 +8,15 @@ phone_number VARCHAR(11) NOT NULL,
 newsletter BOOLEAN NOT NULL DEFAULT false,
 points DECIMAL(6,2),
 PRIMARY KEY(cust_id));
+
+#Add a new column to an existing table
+ALTER TABLE customers ADD age INT NOT NULL;
+
+#Remove a column from an exisiting table
+ALTER TABLE customers DROP COLUMN points;
+
+#Modify an existing column, need to include the constraints you want to keep
+ALTER TABLE customers MODIFY cust_name VARCHAR(80);
 
 SHOW TABLES;
 DESCRIBE customers; #provides a breakdown of the specific table, its columns, data types and constraints
