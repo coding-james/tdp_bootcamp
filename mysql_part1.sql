9f404d4f8fcd3a36ac52f68526fc3719556d6837
coding-james <mail.stroud@gmail.com>
2022-09-21 09:54:17 +0100

    Added further columns to Customer Table

--- mysql_part1.sql
+++ mysql_part1.sql
@@ -3,8 +3,12 @@ USE qa_restaurant; # identifies which table you are going to use
 CREATE TABLE customers(
 cust_id INT UNIQUE NOT NULL AUTO_INCREMENT,
 cust_name VARCHAR(100) NOT NULL,
-phone_number CHAR(11) NOT NULL,
+phone_number VARCHAR(11) NOT NULL,
+newsletter BOOLEAN NOT NULL DEFAULT false,
+points DECIMAL(6,2),
 PRIMARY KEY(cust_id));
 
 SHOW TABLES;
 DESCRIBE customers; #provides a breakdown of the specific table, its columns, data types and constraints
+
+#DROP DATABASE qa_restaurant
