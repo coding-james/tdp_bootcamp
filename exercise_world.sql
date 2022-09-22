4efd55f2f3772e3e350e4172515744d26332f7f4
coding-james <mail.stroud@gmail.com>
2022-09-22 16:50:30 +0100

    Completed World Exercise

--- exercise_world.sql
+++ exercise_world.sql
@@ -31,13 +31,13 @@ SELECT * FROM city WHERE name LIKE "F%" LIMIT 25;
 #SELECT count(ID) FROM city WHERE name LIKE "F%"; # 78 cities begin with F
 
 #Q7 - Using COUNT and JOIN ... ON, get the number of cities in China.
-SELECT COUNT(name) FROM city WHERE CountryCode="CHN";
 SELECT count(city.Name) FROM country JOIN city ON country.code=city.CountryCode WHERE city.CountryCode=(SELECT code FROM country WHERE name="CHINA");
 # 363 cities in China
+#SELECT COUNT(name) FROM city WHERE CountryCode="CHN"; #used to check
 
 #Q8 - Using IS NOT NULL, ORDER BY, and LIMIT, which country has the lowest population? Discard non-zero populations.
-SELECT Name, Population FROM country WHERE Population IS NOT NULL ORDER BY Population ASC LIMIT 1;
-# Antarctica 0
+SELECT Name, Population FROM country WHERE Population IS NOT NULL AND Population!=0 ORDER BY Population ASC LIMIT 1;
+# Pitcairn 50
 
 #Q9 - Using aggregate functions, return the number of countries the database contains.
 SELECT COUNT(Code) FROM country;
@@ -65,16 +65,16 @@ SELECT DISTINCT Language FROM countrylanguage;
 SELECT name, GNP FROM country ORDER BY GNP DESC LIMIT 10;
 
 #16 - List the names of, and number of languages spoken by, the top ten most multilingual countries.
-
+SELECT c.name, COUNT(Language) AS SpokenLang FROM countrylanguage cl JOIN country c ON cl.CountryCode=c.Code GROUP BY cl.CountryCode ORDER BY SpokenLang DESC LIMIT 10;
 
 #17 - List every country where over 50% of its population can speak German.
-
+SELECT c.name, cl.percentage FROM country c JOIN countrylanguage cl ON c.code=cl.CountryCode WHERE cl.Language="German" AND cl.Percentage>50.0 ORDER BY cl.Percentage DESC;
 
 #18 - Which country has the worst life expectancy? Discard zero or null values.
 SELECT name, LifeExpectancy FROM country WHERE LifeExpectancy IS NOT NULL OR LifeExpectancy!=0 ORDER BY LifeExpectancy ASC LIMIT 1;
 
 #19 - List the top three most common government forms.
-SELECT GovernmentForm, COUNT(GovernmentForm) As GF_Count FROM country ORDER BY GF_Count DESC LIMIT 3;
+SELECT GovernmentForm, COUNT(GovernmentForm) As GF_Count FROM country GROUP BY GovernmentForm ORDER BY GF_Count DESC LIMIT 3;
 
 #20 - How many countries have gained independence since records began?
 SELECT COUNT(name) FROM country WHERE IndepYear IS NOT NULL;
