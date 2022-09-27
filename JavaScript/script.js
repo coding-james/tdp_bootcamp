2705a6e38208d03cc00ff67d4567931fe957c695
coding.james <mail.stroud@gmail.com>
2022-09-27 11:13:25 +0100

    js updates

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -44,3 +44,56 @@ console.log(`My Favourite car is ${carMake} and the model is ${carModel}`);
 const formattedText = "formatted message"
 console.log("This text is going to be formatted %c" + formattedText, "color: orange; font-family: fantasy; font-style: bold; background-color: black; padding: 10px;");
 
+// VARIABLES
+//let can change value, called Mutable
+//const can't be changed, called Immutable
+//var no longer used as insecure, was similar to let.
+
+// KEY PAIRS
+let myObject = {
+    key : "value",
+    anotherKey : 12345
+};
+
+console.log(myObject);
+
+let myDetails = {
+    myFirstName : "James",
+    myLastName : "Bond",
+    myCar : "Aston Martin DB5"
+};
+
+console.log(myDetails);
+
+//String interpolation
+//using one string and literals containg placeholders
+//called template literal
+// remeber to use ` instead of "" or ''
+
+let stringCalc = `13 * 12 = ${13 * 12}`;
+console.log(stringCalc);
+
+
+//VARIABLES EXERCISE
+
+// Exercise 2.1: Fint JavaScripts types:
+
+let a;
+let b = "12345";
+let c = 12345;
+let d = true;
+let e = {a: "JavaScript"};
+
+console.log(a + " is a " + typeof(a));
+console.log(b + " is a " + typeof(b));
+console.log(c + " is a " + typeof(c));
+console.log(d + " is a " + typeof(d));
+console.log(e + " is a " + typeof(e));
+
+//  Exercise 2.2: Use template litrals to dynamically change the sentence
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`)
