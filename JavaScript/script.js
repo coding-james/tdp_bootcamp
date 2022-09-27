d38920d3926718af9b8e4fd65b4773d2d9b2a1b6
coding.james <mail.stroud@gmail.com>
2022-09-27 12:45:52 +0100

    more js updates and exercises

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -97,3 +97,134 @@ let moneyPaidSoFar = 2348;
 let totalLeftToPay = totalMoney - moneyPaidSoFar;
 
 console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`)
+
+// ITERATION / LOOPS
+
+// FOR - uses a counter, runs while condition is met
+// i is used to represent iteration
+for(let i = 0; i < 5; i++) {
+    console.log(`i = ${i}`);
+}
+
+
+// WHILE - works with booleans, run until a condition is no longer true
+let condition = true;
+let increment = 0;
+
+while(condition === true) {
+    console.log(`increment = ${increment}`);
+    increment++;
+
+    if(increment === 3) {
+        condition = false;
+    }
+}
+
+
+// DO-WHILE - like a while loop, but will ALWAYS run at least once
+let z = false;
+do {
+    console.log("run forest run");
+} while(z === true);
+
+
+// SWITCH CASES - run until a break or return, can be more effective than large if-else statements
+let num = 1;
+
+switch(num) {
+    case 0:
+        console.log("number is zero");
+        break;
+    case 1:
+        console.log("we are number one");
+    case 4:
+        console.log("we're at 4");
+    case 10:
+        console.log("we made it to ten!");
+        break;
+    default:
+        console.warn("not here");
+}
+
+// Cameron example
+// iterator is equal to 2
+// condition is i<6
+// if true print i + 1
+// then interate i by 2
+// if false stop
+
+// WHILE
+let i = 2;
+while(i<=6) {
+    console.log(i+1);
+    i = i + 2;
+}
+
+// FOR
+for(let i = 2; i <=6; i = i + 2) {
+    console.log(i+1);
+}
+
+// Exercise 3.1 - Recreate the Flow Chart as a Loop
+let A = 100;
+while(A <= 200) {
+    console.log(`A = ${A}`);
+    A++;
+}
+
+// As FOR LOOP
+for (let a = 100; a <= 200; a++) {
+    console.log(`a = ${a}`);
+}
+
+// Exercise 3.2 - Recreate the Flow Chart as a Loop
+let B = 100;
+
+while(B <= 200) {
+    if(B % 2 == 0) {
+        console.log("-");
+    } else {
+        console.log("*");
+    }
+    B++;
+}
+
+// As FOR LOOP
+for (let a = 100; a <= 200; a++) {
+    if (a % 2 == 0) {
+        console.log("loop -");
+    } else {
+        console.log("loop *")
+    }
+}
+
+// Exercise 3.3 - Create a method that can print numbers 1 - 10, 10 times each
+for(let i = 0; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(j);
+    }
+}
+
+// Exercise 3.4 - SWITCH STATEMENT
+let today = new Date();
+let day = today.getDay();
+
+switch(day) {
+    case 0:
+        console.log("It's Sunday");
+        break;
+    case 6:
+        console.log("It's Saturday");
+            break;
+    case 1:
+        console.log(`Happy Monday`);
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log("It's a weekday");
+        break;
+    default:
+        console.warn("You didn't enter a day of the week");
+        break;
+}
