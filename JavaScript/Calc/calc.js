31a54ae4580f76901c1f8b8493cef217cb0d3f80
coding-james <mail.stroud@gmail.com>
2022-09-28 13:55:29 +0100

    Created Calc js

---
+++ JavaScript/Calc/calc.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// Add
+// function add(n1,n2) {
+//     let sum = n1 + n2;
+//     console.log(sum);
+// }
+
+const add = (n1,n2) => console.log((n1 + n2));
+
+add(30,12);
+
+// Subtract
+function subtract(n1, n2) {
+    let sum = n1 - n2;
+    console.log(sum);
+}
+
+subtract(28,12);
+
+// Multiply
+function multiply(n1, n2) {
+    let sum = n1 * n2;
+    console.log(sum);
+}
+
+multiply(5,10);
+
+// Divide
+function divide(n1, n2) {
+    let sum = n1 / n2;
+    console.log(sum);
+}
+
+divide(5,10);
