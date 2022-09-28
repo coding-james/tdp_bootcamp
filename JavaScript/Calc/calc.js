0ee5f7170ea8c8d9f1e0f5cf6bc22bba78b315c1
coding-james <mail.stroud@gmail.com>
2022-09-28 15:09:52 +0100

    updated calc and started advanced

--- JavaScript/Calc/calc.js
+++ JavaScript/Calc/calc.js
@@ -6,9 +6,11 @@
 //     console.log(sum);
 // }
 
+//Add as an Arrow Function
 const add = (n1,n2) => console.log((n1 + n2));
 
 add(30,12);
+
 
 // Subtract
 function subtract(n1, n2) {
@@ -18,6 +20,7 @@ function subtract(n1, n2) {
 
 subtract(28,12);
 
+
 // Multiply
 function multiply(n1, n2) {
     let sum = n1 * n2;
@@ -25,6 +28,7 @@ function multiply(n1, n2) {
 }
 
 multiply(5,10);
+
 
 // Divide
 function divide(n1, n2) {
