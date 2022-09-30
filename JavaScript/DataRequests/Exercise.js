c71fb667d9ea5ffe74d04a57fa7d24349e8668f1
coding-james <mail.stroud@gmail.com>
2022-09-30 12:39:36 +0100

    Data Requests

---
+++ JavaScript/DataRequests/Exercise.js
@@ -0,0 +1,64 @@
+"use strict"
+
+// const URL = "https://reqres.in/api/users";
+const URL = "./Exerise.json";
+// const URL2 = "./trainers.json";
+
+// //fetch GET request from specified URL
+// fetch(URL)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function (data){
+//         console.log(data);
+//     })
+//     .catch(function (err){
+//         console.error();
+//     })
+
+axios.get(URL)
+.then(response => console.log(response.data))
+.catch(err => console.error(err));
+
+// axios.get(URL2)
+// .then(response => console.log(response.data))
+// .catch(err => console.error(err));
+
+axios.get(URL)
+.then(response => appendData(response.data))
+.catch(err => console.error(err));
+    
+//Displaying data on the webpage
+// For each item in the data list, create a divm then card and p with the details
+function appendData(data){
+    let dataList = document.getElementById("myData");
+    for(let i = 0; i<data.length; i++){
+        let trainer = document.createElement("div");
+        let id = document.createElement("card");
+        id.innerText = "ID: " + data[i].id;
+        trainer.appendChild(id);
+        let name = document.createElement("p");
+        name.innerText = "Name: " + data[i].first_name;
+        trainer.appendChild(name);
+        dataList.appendChild(trainer);
+   }
+}
+
+
+
+
+// //Displaying data on the webpage
+// // For each item in the data list, create a divm then card and p with the details
+// function appendData(data){
+//     let dataList = document.getElementById("myData");
+//     for(let i = 0; i<data.length; i++){
+//         let trainer = document.createElement("div");
+//         let id = document.createElement("card");
+//         id.innerText = "ID: " + data[i].userId;
+//         trainer.appendChild(id);
+//         let name = document.createElement("p");
+//         name.innerText = "Name: " + data[i].name;
+//         trainer.appendChild(name);
+//         dataList.appendChild(trainer);
+//     }
+// }
