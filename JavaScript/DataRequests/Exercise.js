c647f7a6632ed29eeb94a037cb67ebe9d9b2ec1b
coding-james <mail.stroud@gmail.com>
2022-09-30 14:00:33 +0100

    Data Request Exercise complete

--- JavaScript/DataRequests/Exercise.js
+++ JavaScript/DataRequests/Exercise.js
@@ -1,7 +1,7 @@
 "use strict"
 
-// const URL = "https://reqres.in/api/users";
-const URL = "./Exerise.json";
+const URL = "https://reqres.in/api/users";
+// const URL = "./Exerise.json";
 // const URL2 = "./trainers.json";
 
 // //fetch GET request from specified URL
@@ -17,7 +17,7 @@ const URL = "./Exerise.json";
 //     })
 
 axios.get(URL)
-.then(response => console.log(response.data))
+.then(response => console.log(response.data.data))
 .catch(err => console.error(err));
 
 // axios.get(URL2)
@@ -25,7 +25,7 @@ axios.get(URL)
 // .catch(err => console.error(err));
 
 axios.get(URL)
-.then(response => appendData(response.data))
+.then(response => appendData(response.data.data))//second .data is to allow it to get in the next set of brackets
 .catch(err => console.error(err));
     
 //Displaying data on the webpage
@@ -33,13 +33,29 @@ axios.get(URL)
 function appendData(data){
     let dataList = document.getElementById("myData");
     for(let i = 0; i<data.length; i++){
-        let trainer = document.createElement("div");
-        let id = document.createElement("card");
+        
+        let trainer = document.createElement("div"); //create div
+        
+        let id = document.createElement("card"); //create card with id
         id.innerText = "ID: " + data[i].id;
         trainer.appendChild(id);
-        let name = document.createElement("p");
+
+        let name = document.createElement("p"); //create p with first name
         name.innerText = "Name: " + data[i].first_name;
         trainer.appendChild(name);
+
+        let lastName = document.createElement("p"); //create p with last name
+        lastName.innerText = "Surname: " + data[i].last_name;
+        trainer.appendChild(lastName);
+        
+        let email = document.createElement("p"); //create p with email
+        email.innerText = "Email: " + data[i].email;
+        trainer.appendChild(email);
+
+        let image = document.createElement("img"); //create img with avatar
+        image.src = data[i].avatar;
+        trainer.appendChild(image);
+
         dataList.appendChild(trainer);
    }
 }
