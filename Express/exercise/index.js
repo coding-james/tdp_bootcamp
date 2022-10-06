e78364a2b6ebc3e2c7e3aa00ef2c3b26a704ff84
coding-james <mail.stroud@gmail.com>
2022-10-06 16:29:13 +0100

    end of day

--- Express/exercise/index.js
+++ Express/exercise/index.js
@@ -1,19 +1,28 @@
 "use strict"
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); //required for post request
 
 const app = express ();
 
 app.use(express.json());
+// app.use(logTime); //Don't need this
 
-let names = ["James Bond", "Jason Bourne", "Austin Powers", "Ethan Hunt"];
+let names = ["James Bond", "Jason Bourne", "Austin Powers", "Ethan Hunt", "Dana Scully", "M"];
 
 // Get Request
 // app.get("/read", (req, res) => console.log("fetch"));
 app.get('/', (req, res) => res.send("Hello, my name is Bond, James Bond"));
 
+//Middleware - Logs Date and Time
+const logTime = (req, res, next) => {
+    console.log("Time stamp:", new Date());
+    next();
+}
+
 // Get All Users
-app.get("/allUsers", (req, res) => res.send(names));
+// in postman example - localhost:4494/allUsers
+// includes prompt to run Middleware
+app.get("/allUsers", logTime, (req, res) => res.send(names));
 
 //Get a specific User by index using URL parameter
 app.get("/User/:id", (req, res) => res.send(names[req.params.id]));
@@ -21,19 +30,20 @@ app.get("/User/:id", (req, res) => res.send(names[req.params.id]));
 // Post Request
 // app.post("/create", (req, res) => console.log("create"));
 app.post('/create', (req, res) => {
-    const name = req.body.name;
-    names.push(name);
-    res.status(201).send(`${name} added successfully`);
+    const newName = req.body.name;
+    names.push(newName);
+    res.status(201).send(`${newName} added successfully`);
 });
 
 // Post Request
 // app.put("/replace", (req, res) => console.log("replace"));
-app.put('/replace/:index', (req, res) => {
-    const name = req.query.name;
-    const index = req.params.index;
-    const old = names[index];
-    names[index] = name;
-    res.status(202).send(`${old} successfully replaced with ${name}`);
+//  in postman example - localhost:4494/replace/2?name=Jon
+app.put('/replace/:id', (req, res) => {
+    console.log("QUERY:", req.query);
+    const newName = req.query.name;
+    const oldName = names[req.params.id];
+    names[req.params.id] = newName;
+    res.status(202).send(`${oldName} successfully replaced with ${newName[req.params.id]}`);
 });
 
 // Patch Request
@@ -41,6 +51,7 @@ app.put('/replace/:index', (req, res) => {
 
 // Delete Request
 // app.delete("/delete/:id", (req, res) => console.log("delete:", req.params.id));
+//in postman example - localhost:4494/delete/1
 app.delete('/delete/:id', (req, res) => {
     res.send(names.splice(req.params.id, 1));
 });
