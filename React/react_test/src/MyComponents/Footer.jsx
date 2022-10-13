49a08d7bebbfc6035ebbd8933694c9a71e3b3a21
coding-james <mail.stroud@gmail.com>
2022-10-13 12:24:41 +0100

    More React and Pre-Lunch

---
+++ React/react_test/src/MyComponents/Footer.jsx
@@ -0,0 +1,12 @@
+const AnotherComponent = () => {
+    return (
+        <div>
+            <p>Hello World</p>
+            <p>Line 2 of text</p>
+            <p>Line 3</p>
+            <p>Line 4</p>
+        </div>      
+    );
+};
+
+export default AnotherComponent;
