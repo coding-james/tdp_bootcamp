f10e684156aa6d8cacd4d8f4339d03f140a2acff
coding-james <mail.stroud@gmail.com>
2022-10-18 15:01:10 +0100

    Added Qty to array and ability to change the qty

--- React/react_test/src/MyComponents/LiftingState/SearchList.jsx
+++ React/react_test/src/MyComponents/LiftingState/SearchList.jsx
@@ -8,7 +8,7 @@ function SearchList() {
 
   const addNewItem = (event) => {
     event.preventDefault();
-    setItems((currentItems) => [...currentItems, { name: newItem, qty: 1 }]); // ?
+    setItems((currentItems) => [...currentItems, { name: newItem, qty: 1 }]); // When an item created, default qty to 1
     setNewItem('');
   };
 
@@ -16,6 +16,22 @@ function SearchList() {
     const cloneItems = [...items];
     cloneItems.splice(index, 1);
     setItems(cloneItems);
+  };
+
+  const increaseQty = (index) => {
+    const cloneItems = [...items];
+    cloneItems.splice(index, 1, { name: cloneItems[index].name, qty: (cloneItems[index].qty + 1) });
+    setItems(cloneItems);
+  };
+
+  const decreaseQty = (index) => {
+    const cloneItems = [...items];
+    if(cloneItems[index].qty > 1){
+      cloneItems.splice(index, 1, { name: cloneItems[index].name, qty: (cloneItems[index].qty - 1) });
+      setItems(cloneItems);
+    } else {
+      removeItem(); //if user decreases to zero, remove item
+    }
   };
 
   return (
@@ -27,7 +43,8 @@ function SearchList() {
         <button type="submit">Add New</button>
       </form>
       <br />
-      <label>Search <SearchBar text={search} changeHandler={(e) => setSearch(e.target.value)} /></label>
+      {/* OLD CODE PRIOR TO QTY been included in the array */}
+      {/* <label>Search <SearchBar text={search} changeHandler={(e) => setSearch(e.target.value)} /></label>
       {
             items
               .filter((item) => item.toLowerCase().startsWith(search.toLowerCase()))
@@ -37,7 +54,26 @@ function SearchList() {
                   <button type="button" onClick={() => removeItem(i)}>X</button>
                 </p>
               ))
-        }    
+        }     */}
+
+      <label>Search <SearchBar text={search} changeHandler={(e) => setSearch(e.target.value)} /></label>
+      {
+        items
+          .filter((item) => item.name.toLowerCase().startsWith(search.toLowerCase()))
+          .map((item, i) => (
+            <p>
+              {`${item.name} Qty: ${item.qty}`}
+              <button type="button" onClick={() => increaseQty(i)}>Qty +</button>
+              <button type="button" onClick={() => decreaseQty(i)}>Qty -</button>
+              <button type="button" onClick={() => removeItem(i)}>X</button>
+            </p>
+          ))
+      }
+
+      {/* OLD CODE - Figuring out how to display the array */}
+      {/* {items.map(item => (
+          <p>{item.name} Qty: {item.qty}</p>
+        ))} */}
 
     </div>
   );
